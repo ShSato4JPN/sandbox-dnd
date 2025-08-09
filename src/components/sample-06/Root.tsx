521a4346a24eefd228932b4da7985c22b13053d5
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-08-09 16:12:54 +0900

    fix: splitting components

--- src/components/sample-06/Root.tsx
+++ src/components/sample-06/Root.tsx
@@ -1,25 +1,37 @@
 import DndProvider from "./provider/dnd";
 import { createMenu } from "./util/create-menu";
 
-export default function Root() {
+function DraggableMenu() {
   const menus = createMenu(10);
 
   return (
+    <div>
+      <ul className="flex flex-col gap-2">
+        {menus.map((menu) => (
+          <li
+            className="border-1 rounded-lg p-2 cursor-pointer hover:border-red-500"
+            key={menu.id}
+          >
+            {menu.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function DroppableArea() {
+  return (
+    <div className="border-1 rounded-lg min-w-[500px] bg-gray-700 opacity-40" />
+  );
+}
+
+export default function Root() {
+  return (
     <DndProvider>
       <div className="grid grid-cols-2 gap-10 border-1 p-8 rounded-lg">
-        <div>
-          <ul className="flex flex-col gap-2">
-            {menus.map((menu) => (
-              <li
-                className="border-1 rounded-lg p-2 cursor-pointer hover:border-red-500"
-                key={menu.id}
-              >
-                {menu.label}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="border-1 rounded-lg min-w-[500px] bg-gray-700 opacity-40" />
+        <DraggableMenu />
+        <DroppableArea />
       </div>
     </DndProvider>
   );
