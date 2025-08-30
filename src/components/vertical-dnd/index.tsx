613c7296ca928ef5788198271c27f54c5d7d3963
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-08-30 20:01:26 +0900

    feat: add chips value components

--- src/components/vertical-dnd/index.tsx
+++ src/components/vertical-dnd/index.tsx
@@ -1,3 +1,101 @@
+import {
+  closestCenter,
+  DndContext,
+  DragOverlay,
+  PointerSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { useState } from "react";
+import SortableItem from "./SortableItem";
+
+const items = [
+  {
+    id: "item-1",
+    value: "value1",
+  },
+  {
+    id: "item-2",
+    value: "value2",
+  },
+  {
+    id: "item-3",
+    value: "value3",
+  },
+  {
+    id: "item-4",
+    value: "value4",
+  },
+  {
+    id: "item-5",
+    value: "value5",
+  },
+];
+
 export default function Root() {
-  return <div>index</div>;
+  const [chips, setChips] = useState<{ id: string; value: string }[]>(items);
+  const [selectedChip, setSelectedChip] = useState<string | null>(null);
+
+  const sensors = useSensors({
+    sensor: PointerSensor,
+    options: {
+      activationConstraint: {
+        distance: 8, // 8px動かしたらドラッグ開始
+      },
+    },
+  });
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={(event) => {
+        const { active, over } = event;
+        if (over == null || active.id === over.id) {
+          return;
+        }
+        const oldIndex = chips.findIndex((v) => v.id === active.id);
+        const newIndex = chips.findIndex((v) => v.id === over.id);
+        const newChips = arrayMove(chips, oldIndex, newIndex);
+        setChips(newChips);
+      }}
+    >
+      <div className="flex flex-row gap-10">
+        <div className="flex flex-col gap-2">
+          <SortableContext
+            items={chips.map((v) => v.id)}
+            strategy={verticalListSortingStrategy}
+          >
+            {chips.map((chip) => (
+              <SortableItem
+                key={chip.id}
+                id={chip.id}
+                isSelected={selectedChip === chip.id}
+                handleClick={() => setSelectedChip(chip.id)}
+              >
+                <span>item-{chip.id}</span>
+              </SortableItem>
+            ))}
+          </SortableContext>
+        </div>
+        <div className="flex flex-row gap-10">
+          {chips.map((v) => (
+            <div
+              key={v.id}
+              className="grid place-items-center p-4 border-dashed border rounded-lg"
+            >
+              {v.value}
+            </div>
+          ))}
+        </div>
+      </div>
+      <DragOverlay>
+        <div className="p-2 border rounded-lg bg-orange-200">tests</div>
+      </DragOverlay>
+    </DndContext>
+  );
 }
